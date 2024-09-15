eb92eb9d4e83f53c4faf3170eb90e0dfd1370898
Giseldo Neo <giseldo@gmail.com>
2024-09-14 23:36:27 -0400

    ultima versao

--- app.py
+++ app.py
@@ -1,36 +1,25 @@
 import os
-
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+  return render_template('login.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/login', methods=['GET', 'POST']) 
+def login(): 
+    return render_template('home.html') 
 
-@app.route('/ola', methods=['GET'])
-def ola():
-    return "Ola mundo"
+@app.route('/submit_details', methods=['GET', 'POST']) 
+def submit_details(): 
+    return redirect(url_for('index')) 
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+@app.route('/contacts', methods=['GET']) 
+def contacts(): 
+    contacts = "" 
+    return render_template('contacts.html', contacts=contacts) 
 
 if __name__ == '__main__':
    app.run()
